9638ec28ec41c7b916f9cd35f4debc178b28d10a
laptop <oaclc@clc330-003ml.local>
2019-03-13 11:40:07 -0400

    Initial Commit

---
+++ Uno Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Uno Game
+//
+//  Created by laptop on 2019-03-13.
+//  Copyright © 2019 laptop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Uno GameTests/Uno_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Uno_GameTests.swift
+//  Uno GameTests
+//
+//  Created by laptop on 2019-03-13.
+//  Copyright © 2019 laptop. All rights reserved.
+//
+
+import XCTest
+@testable import Uno_Game
+
+class Uno_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
